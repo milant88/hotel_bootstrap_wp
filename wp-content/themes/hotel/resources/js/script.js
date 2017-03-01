efbf668f3fcef7e441419f9da45f89be40b26b84
Milan Tomic <milan.tomic.rs@gmail.com>
2017-03-01 13:24:35 +0100

    blog page added, blog section finished

--- wp-content/themes/hotel/resources/js/script.js
+++ wp-content/themes/hotel/resources/js/script.js
@@ -3,3 +3,18 @@ $('#topPage').click(function(){
     return false;
  });
 
+function isPalindrom(str) {
+    return (str == str.split('').reverse().join(''));
+}
+
+
+function proveri(str) {
+    var k = str.length-1;
+    var temp;
+    for(var i = 0; i < str.length;i++) {
+        temp[i] = str[k];
+        k--;
+    }
+    return str == temp;
+
+}
